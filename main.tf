4e3cf9d2b973a48c914046a7c73fb3881a801af8
Eric Reeves <eric@alluvium.com>
2024-02-04 15:37:23 -0600

    Create app1 secret

--- main.tf
+++ main.tf
@@ -1,9 +1,38 @@
 provider "vault" {
 }
 
+variable "app1-api-key" {
+  description = "App1 Api Key"
+}
+
 #--------------------------------------
-# Create 'admin/alluvium' namespace
+# Create 'app1-team' Namespace
 #--------------------------------------
-resource "vault_namespace" "test" {
-  path = "test9"
+resource "vault_namespace" "app1-team" {
+  path = "app1-team"
+}
+
+resource "vault_mount" "app1" {
+  path        = "app1-secrets"
+  type        = "kv"
+  options     = { version = "2" }
+  description = "App1 Secrets"
+}
+
+resource "vault_kv_secret_v2" "app1-api-key" {
+  mount = vault_mount.app1.path
+  name  = "api-key"
+  data_json = jsonencode(
+    {
+      endpoint = "http://api.app1.example.com",
+      key      = var.app1-api-key
+    }
+  )
+  custom_metadata {
+    max_versions = 10
+    data = {
+      owner = "eric.reeves@hashicorp.com",
+      team  = "Platform"
+    }
+  }
 }
