2f959616d65baf9d71ca4581c916175a73295f91
Eric Reeves <eric@alluvium.com>
2024-02-04 21:38:06 -0600

    Add azure_secrets workspace access

---
+++ azure_secrets_workspace_access.tf
@@ -0,0 +1,65 @@
+##########################################################
+# Configuration
+##########################################################
+locals {
+  tfc_labs_role          = "tfc-labs-role"
+  tfc_labs_auth_path     = "jwt-labs"
+  azure_secrets_vcs_repo = "cube-earth-labs/azure_secrets"
+}
+
+resource "vault_jwt_auth_backend" "tfc_jwt" {
+  path               = local.tfc_labs_auth_path
+  type               = "jwt"
+  oidc_discovery_url = "https://app.terraform.io"
+  bound_issuer       = "https://app.terraform.io"
+}
+
+resource "vault_jwt_auth_backend_role" "tfc_role" {
+  backend        = vault_jwt_auth_backend.tfc_jwt.path
+  role_name      = local.tfc_labs_role
+  token_policies = [vault_policy.tfc_policy.name]
+
+  bound_audiences = ["vault.workload.identity"]
+
+  bound_claims_type = "glob"
+  bound_claims = {
+    sub = "organization:ericreeves-demo:project:Cube Earth Labs:workspace:azure_secrets:run_phase:*"
+  }
+
+  user_claim = "terraform_full_workspace"
+  role_type  = "jwt"
+  token_ttl  = 1200
+}
+
+resource "vault_policy" "tfc_policy" {
+  name = "tfc-policy"
+
+  policy = <<EOT
+# Allow tokens to query themselves
+path "auth/token/lookup-self" {
+  capabilities = ["read"]
+}
+
+# Allow tokens to renew themselves
+path "auth/token/renew-self" {
+    capabilities = ["update"]
+}
+
+# Allow tokens to revoke themselves
+path "auth/token/revoke-self" {
+    capabilities = ["update"]
+}
+
+path "sys/mounts" {
+  capabilities = ["list", "read"]
+}
+
+path "sys/mounts/example" {
+  capabilities = ["create", "read", "update", "patch", "delete", "list"]
+}
+
+path "azure/creds/*" {
+  capabilities = ["read"]
+}
+EOT
+}
